30c01ccaeedaf79f2fdf5ba510aa0e98c1bc4baf
Arnav Garg <arnavgrg@gmail.com>
2019-01-12 16:10:07 -0800

    Updated lab0.c and README for Lab-0

--- Lab-0/lab0.c
+++ Lab-0/lab0.c
@@ -60,18 +60,19 @@ int main(int argc, char* argv[]){
         {0,0,0,0}
     };
 
-    //Variables to keep track of 
-    char* infile_name = NULL;
-    char* outfile_name = NULL;
+    //Variables to keep track of:
     int catch_flag = 0;
-    int seg_flag = 0;
-    int dump_flag = 0;
     int choice, outfile_fd, infile_fd;
 
     //int getopt(int argc, char *const argv[], const char *optstring)
     /*int getopt_long(int argc, char * const argv[], const char *optstring, 
                         const struct option *longopts, int *longindex);*/
     //More info: http://man7.org/linux/man-pages/man3/getopt.3.html
+    /*getopt_long works as follows:
+        1. If no options, returns -1 and skips while loop.
+        2. If options, checks if all flags are valid. If yes, then
+        3. Processes each flag in order from left to right
+    */
     while ((choice = getopt_long(argc,argv,"",choices,NULL)) != -1){
         /*Set flags and options correctly so they can be used after the while
         loop*/
@@ -79,115 +80,90 @@ int main(int argc, char* argv[]){
             case input: 
                 /*optarg contains argument for the given flag*/
                 /*incase of error/failure, optarg is set to null*/
-                infile_name = optarg;
+                if (optarg) {
+                    infile_fd = open(optarg, O_RDONLY);
+                    /* 
+                    Spec: File Descriptor Management
+                    int ifd = open(newfile, O_RDONLY);
+                    if (ifd >= 0) {
+	                    close(0);
+	                    dup(ifd);
+	                    close(ifd);
+                    }
+                    */
+                    if (infile_fd >= 0){
+                        //Close stdin and destroy its file table entry
+                        close(0);
+                        //Set infile file_descriptor to 0 (stdin)
+                        dup2(infile_fd,0);
+                        //Close infile_fd
+                        close(infile_fd);
+                    } 
+                    else {
+                        /*If no file found, print to stderr and exit with status 2*/
+                        /*errno is set by system calls and some library functions in the event 
+                        of an error to indicate what went wrong. Don't explicitly define it. */
+                        /*sterror returns a pointer to a string that describes
+                        the error code passed in the argument*/
+                        //Else, infile_fd == -1 which indicates an error while opening the file
+                        fprintf(stderr, "Cannot open input file: %d - %s\n", errno, strerror(errno));
+				        exit(2);
+                    }     
+                }
                 break;
             case output:
-                outfile_name = optarg;
+                if (optarg) {
+                    // Check if file exists and create the file specified 
+                    // if file exists, it is automatically truncated
+                    // Set permissions for the file created to rw-
+                    // creat is the same as open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                    outfile_fd = creat(optarg, 0666);
+                    /* 
+                    Spec: File Descriptor Management
+                    int ofd = creat(newfile, 0666);
+                    if (ofd >= 0) {
+	                    close(1);
+	                    dup(ofd);
+	                    close(ofd);
+                    }
+                    */
+                    //If the returned file descriptor is >= 0
+                    if (outfile_fd >= 0){
+                        //Close stdout and destroy its file table entry
+                        close(1);
+                        //Set outfile's fd to 1 (stdout) + silently close 
+                        dup2(outfile_fd, 1);
+                        //Close outfile_fd
+                        close(outfile_fd);
+                    } else {
+                        //File could not be created/truncated. Print to stderr and exit with status 3.
+                        fprintf(stderr, "Cannot create or truncate output file:  %d - %s\n", errno, strerror(errno));
+                        exit(3);
+                    }
+                }
                 break;
             case segf:
-                seg_flag = 1;
+                //Call the createSegfault() method to immediately cause a segfault.
+                createSegfault();
                 break;
             case catch:
+                //Set catch_flag to 1 (for dump case)
                 catch_flag = 1;
+                /* If a segfault is called after this line, instead of breaking out of the
+                program or going into an infinity loop (depending on the OS), it calls 
+                the segfault_handler method to handle the segmentation fault */
+                signal(SIGSEGV, segfault_handler);
                 break;
             case dump:
-                dump_flag = 1;
+                /*If dump-core is passed in by itself, do nothing
+                If segfault is passed in before dump-core, doesn't matter
+                If catch is passed in before dump-core, call dumpCore method.*/
+                if (catch_flag){dumpCore();}
                 break;
             default:
                 //If wrong/unrecognized flag is passed in, call the icorrectOptions method
                 incorrectOptions();
                 break;
-        }
-    }
-
-    //csd
-    //000 -> Already taken care of this situation
-    //001 -> if only dump, pretty pointless because there's no point in dumping the core
-    //010 -> if only segfault, then create segfault with RC: 139
-    //011 -> if segfault + dump, then we want to dump the core after causing a segfault
-    //100 -> if just catch, raise the signal but results in an infinite loop
-    //101 -> if catch and dump, essentially do nothing 
-    //110 -> catch + seg -> return exit code 4
-    //111 -> catch + seg + dump
-
-    //Creates a segfault signal and calls the segfault_handler method. 
-    //If no handler method specified, the signal goes into an inifinite loop by default.
-    if (catch_flag) {
-        //Create signal so program knows how to handle segfaults from here
-        signal(SIGSEGV, segfault_handler);
-    }
-
-    //If dump_flag set to 1, check if catch or segfault are also passed as arguments.
-    //If yes, call dumpCore(), otherwise do nothing because we don't want to exit prematurely.
-    if (dump_flag) {
-       if (catch_flag || seg_flag) {
-            dumpCore();
-       }
-    }
-
-    //If seg_flag set to 1, create a segmentation fault
-    if (seg_flag) {
-        createSegfault();
-    }
-
-    if (infile_name) {
-        infile_fd = open(infile_name, O_RDONLY);
-            /* 
-            Spec: File Descriptor Management
-            int ifd = open(newfile, O_RDONLY);
-                if (ifd >= 0) {
-	                close(0);
-	                dup(ifd);
-	                close(ifd);
-                }
-            */
-            if (infile_fd >= 0){
-                //Close stdin and destroy its file table entry
-                close(0);
-                //Set infile file_descriptor to 0 (stdin)
-                dup2(infile_fd,0);
-                //Close infile_fd
-                close(infile_fd);
-            }
-            else {
-                /*If no file found, print to stderr and exit with status 1*/
-                /*errno is set by system calls and some library functions in the event 
-                of an error to indicate what went wrong. Don't explicitly define it. */
-                /*sterror returns a pointer to a string that describes
-                the error code passed in the argument*/
-                //Else, infile_fd == -1 which indicates an error while opening the file
-                fprintf(stderr, "Cannot open input file: %d - %s", errno, strerror(errno));
-				exit(2);
-        }     
-    }
-
-    if (outfile_name) {
-        // Check if file exists and create the file specified 
-        // if file exists, it is automatically truncated
-        // Set permissions for the file created to rw-
-        // creat is the same as open(filepath, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-        outfile_fd = creat(outfile_name, 0666);
-        /* 
-        Spec: File Descriptor Management
-        int ofd = creat(newfile, 0666);
-            if (ofd >= 0) {
-	        close(1);
-	        dup(ofd);
-	        close(ofd);
-        }
-        */
-        //If the returned file descriptor is >= 0
-        if (outfile_fd >= 0){
-            //Close stdout and destroy its file table entry
-            close(1);
-            //Set outfile's fd to 1 (stdout) + silently close 
-            dup2(outfile_fd, 1);
-            //Close outfile_fd
-            close(outfile_fd);
-        } else {
-            //File could not be created/truncated. Print to stderr and exit with status 2.
-            fprintf(stderr, "Cannot create or truncate output file:  %d - %s", errno, strerror(errno));
-            exit(3);
         }
     }
 
@@ -200,20 +176,20 @@ int main(int argc, char* argv[]){
     while ((r = read(0,buffer,sizeof(char))) > 0) {
         //Check for read-in errorr or signal interrupt
         if (r == (size_t)-1) {
-            fprintf(stderr, "Error reading from file/stdout: %s", strerror(errno));
+            fprintf(stderr, "Error reading from file/stdout: %s\n", strerror(errno));
             exit(5);
         }
         //Write one byte to stdout
         w = write(1,buffer,sizeof(char));
         //Check if write system call fails and print to stderr
         if ((int)w < 0){
-            fprintf(stderr, "Unable to write to stdout: %d - %s", errno, strerror(errno));
+            fprintf(stderr, "Unable to write to stdout: %d - %s\n", errno, strerror(errno));
             exit(6);
         } 
         //Number of bytes read should equal number of bytes written
         else if (w != r) {
             fprintf(stderr,"Number of bytes read from stdin does not equal number of bytes written to stdout. \
-                        Bytes read: %d; Bytes written: %d",(int)r,(int)w);
+                        Bytes read: %d; Bytes written: %d\n",(int)r,(int)w);
             exit(7);
         }
     }
@@ -239,20 +215,22 @@ void createSegfault() {
 }
 
 //Print to stderr and exit if segfault is caught
-void segfault_handler(int num){
+void segfault_handler(int num) {
     if (num == SIGSEGV){
-        fprintf(stderr, "SIGSEGV signal caught: error number: %d; message: %s", errno, strerror(errno));
+        fprintf(stderr, "SIGSEGV signal caught: error number: %d; message: %s\n", errno, strerror(errno));
         exit(4);
     }
 }
 
-//Dump core used to reset Catch
-void dumpCore(){
-    exit(139);
+//dumpCore method
+void dumpCore() {
+    /*reset signal for segmentation fault to default 
+    instead of calling segfault_handler and exit with status 139*/
+    signal(SIGSEGV, SIG_DFL);
 }
 
 //If wrong flag/option is passed into the program
-void incorrectOptions(){
-    printf("Only valid options are --input=filename, --output=filename, --catch, --segfault and --dump-core");
+void incorrectOptions() {
+    fprintf(stderr, "Only valid options are --input=filename, --output=filename, --catch, --segfault and --dump-core\n");
     exit(1);
 }
