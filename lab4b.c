4a9247438b1f75672e9f33372f8584466363499d
Arnav Garg <arnavgrg@gmail.com>
2019-03-07 10:31:00 -0800

    Update lab4b.c

--- lab4b.c
+++ lab4b.c
@@ -6,6 +6,40 @@
 #include <stdio.h>
 #include <getopt.h>
 
+#define PERIOD 'p'
+#define SCALE  's'
+
+#Initialize a global static variable called period to 1
+static int period = 1
+static char scale = 'F'
+
 int main(int argc, char* argv[]){
+    //Needed for getopt_long
+    int choice;
+    //Set option_index for getopt_long
+    int option_index = 0;
   
+    static struct option choices[] = {
+      {"period", optional_argument, NULL, PERIOD},
+      {"scale",  optional_argument, NULL, SCALE},
+      {0,0,0,0}
+    }
+  
+    while ((choice = getopt_long(argc, argv, "", choices, &option_index)) != -1) {
+      switch(choice) {
+        case PERIOD: 
+          if (optarg)
+              period = atoi(optarg);
+          break;
+        case SCALE: 
+          if (optarg != 'C' || optarg != 'F'){
+           fprintf(stderr, "Invalid argument passed in for scale. Only valid options are 'C' and 'F'\n");
+           fflush(stderr);
+           exit(1);
+          } 
+          scale = optarg
+          break;
+        default: break;
+      }
+    }
 }
