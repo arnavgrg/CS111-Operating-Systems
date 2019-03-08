334e1430b9bd6893576d2408db47303911646f08
Arnav Garg <arnavgrg@gmail.com>
2019-03-08 10:49:54 -0800

    Update lab4b.c

--- lab4b.c
+++ lab4b.c
@@ -14,19 +14,30 @@
 
 #define PERIOD 'p'
 #define SCALE  's'
-#define STOP   'x'
-#define START  'a'
 #define LOG    'l'
-#define OFF    'o'
 
 //Initialize a global static variable called period to 1
 static int period = 1;
 static char scale = 'F';
-static char* log_file = NULL;
+FILE* log_file = NULL;
 
 //Create instances of the Sensor & Button
 mraa_aio_context sensor;
 mraa_gpio_context button;
+
+//Print errors and exit
+void print_error(char* err){
+  fprintf(stderr, "%s\n", err);
+  fflush(stderr);
+  exit(1);
+}
+
+//Print errors with errno
+void print_error_with_errno(char* err, int error_num){
+    fprintf(stderr, "%s: %d - %s\n", err, error_num, strerror(error_num));
+    fflush(stderr);
+    exit(1);
+}
 
 //Initialize temperature sensor
 void initialize_sensor(){
@@ -35,11 +46,8 @@ void initialize_sensor(){
     //Grove Temperature Sensor to the Analog A0/A1 connector on your Grove cape, where it will be addressed as I/O pin #1.
     sensor = mraa_aio_init(0);
     //Check if the sensor failed to initialize
-    if (!sensor){
-       fprintf(stderr, "Temperature sensor failed to initialize correctly: %d - %s\n", errno, strerror(errno));
-       fflush(stderr);
-       exit(1);
-    }
+    if (!sensor)
+       print_error_with_errno("Temperature sensor failed to initialize correctly", errno);
 }
 
 //Initialize button
@@ -51,38 +59,32 @@ void initialize_button(){
     //Grove Button attached to the GPIO_50 connector on Grove cape, where it will be addressed as I/O pin #60.
     button = mraa_gpio_init(60);
     //Check if the button failed to initialize
-    if (!button){
-       fprintf(stderr, "Button failed to initialize correctly: %d - %s\n", errno, strerror(errno));
-       fflush(stderr);
-       exit(1);
-    }
+    if (!button)
+       print_error_with_errno(stderr, "Button failed to initialize correctly", errno);
+    //Set button to be input
+    mraa_gpio_dir(button, MRAA_GPIO_IN);
 }
 
 //Convert reading into a temperature
 //http://wiki.seeedstudio.com/Grove-Temperature_Sensor_V1.2/
-float temp_convert(float a){
+float getTemperature(float a){
     int B = 4275;
     int R_0 = 100000;
     float R = R_0 * (1023.0/(a-1.0);
-    return 1.0/(log(R/R_0)/B+1/298.15)-273.15;
-}
-
-//Temperature is in farhenheit by default
-//Function to convert farhenheit to celcius
-float f_to_c (float f){
-    return (f-32/1.8);
+    float result = 1.0/(log(R/R_0)/B+1/298.15)-273.15;
+    if (strcmp(scale,'F') == 0)
+       return result*1.8 + 32;
+    return result;
 }
 
 //Function to get time in the correct format
+//For this function to work correctly, we need to configure the local timezone on our BeagleBone.
 void get_time(char *real_time){
     time_t current;
     time(&current);
     struct tm *localTimeNow = localtime(&current);
-    if (!localTimeNow) {
-        fprintf(stderr, "Failure initializing struct tm\n");
-        fflush(stderr);
-        exit(1);
-    }
+    if (!localTimeNow)
+        print_error("Failure initializing struct tm");
     strftime(real_time, 9, "%H:%M:%S", localTimeNow);
     free(localTimeNow);
 }
@@ -91,6 +93,7 @@ void get_time(char *real_time){
 int main(int argc, char* argv[]){
     //Needed for getopt_long
     int choice;
+    
     //Set option_index for getopt_long
     int option_index = 0;
     
@@ -98,55 +101,50 @@ int main(int argc, char* argv[]){
     static struct option choices[] = {
       {"period", optional_argument, NULL, PERIOD},
       {"scale",  optional_argument, NULL, SCALE},
-      {"stop",   no_argument,       NULL, STOP},
-      {"start",  no_argument,       NULL, START},
-      {"log",    optional_argument, NULL, LOG},
-      {"off",    no_argument,       NULL, OFF},
+      {"log",    required_argument, NULL, LOG},
       {0,0,0,0}
     }
-  
+    
     //Main getopt while loop to process CLI arguments    
     while ((choice = getopt_long(argc, argv, "", choices, &option_index)) != -1) {
-      switch(choice) {
+        switch(choice) {
         //Case Period: sampling interval
         case PERIOD: 
-          if (atoi(optarg) < 0){
-              fprintf(stderr, "Period cannot be a negative value\n");
-              fflush(stderr);
-          }
+          if (atoi(optarg) < 0)
+              print_error("Period cannot be a negative value");
           if (optarg)
               period = atoi(optarg);    
           break;
         //Case to process whether the decided scale is F or C
         case SCALE: 
-          if (strcmp(optarg,'C') == 0)
+          if (strlen(optarg) > 1)
+            print_error("Too many characters specified for scale");
+          else if (strcmp(optarg,'C') == 0)
             scale = 'C';
-          else if (strcmp(optarg, 'F') == 0)
+          else if (strcmp(optarg,'F') == 0)
             scale = 'F';
-          else {
-           fprintf(stderr, "Invalid argument passed in for scale. Only valid options are 'C' and 'F'\n");
-           fflush(stderr);
-           exit(1);
-          } 
+          else
+            print_error("Invalid argument passed in for scale. Only valid options are 'C' and 'F'");
           break;
-        //Case STOP
-        case STOP: break;
-        //Case START
-        case START: break;
-        //Case OFF      
-        case OFF: break;
-        //CASE LOG
         case LOG: 
           if (optarg)
-              log_file = optarg;
+              //Try opening the filestream
+              log_file = fopen(optarg, "a+");
+              if (!log_file)
+                print_error("Failed to open log file correctly");
           break;
         //Default case if invalid/incorrect arguments passed in
         default: 
           fprintf(stderr, "Invalid argument %s passed in\n", optarg);
           fflush(stderr);
+          exit(1);
           break;
       }
     }
+    
+    //Detects running platform and attempts to use included pinmap, this is run on module/library init/load 
+    //but is handy to rerun to check board initialised correctly.
+    mraa_init();
     
     //Try initializing both the sensors
     initialize_sensor();
